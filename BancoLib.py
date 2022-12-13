22fad74f331b505af87b45dbb4b254c1d166a29d
Enock <94856177+eeenock@users.noreply.github.com>
2022-12-13 06:23:15 +0000

    Atividade 2 - Banco Melhorado

--- BancoLib.py
+++ BancoLib.py
@@ -7,7 +7,8 @@ class Conta():
         self.saldo = 0
 
     def deposite(self, valor):
-        self.saldo = self.saldo + valor
+        desconto = valor * 0.001 
+        self.saldo = self.saldo + valor - desconto
 
     def sacar(self, valor):
         if self.saldo >= valor:
@@ -20,7 +21,13 @@ class Conta():
 class Poupanca(Conta):
 
     def render(self):
-        self.saldo = self.saldo + self.saldo*0.01
+        self.saldo = self.saldo + self.saldo*0.001
+
+class contaBonificada(Conta) :
+
+    def render(self):
+          self.saldo = self.saldo + self.bonus
+          self.bonus = 0 
 
 
 class Banco():
@@ -43,25 +50,102 @@ class Banco():
         self.contas.append(p)
         return num
 
+    def criarContaBonificada(self):
+        num = random.randint(0, 1000)
+        cB = contaBonificada(num)
+        self.contas.append(cB)
+        cB.bonus = 0
+        return num
+
+    def buscaConta(self,numConta):
+        existente = 0
+        conta = 0
+        for i in self.contas:
+            if i.numero == numConta :
+               conta = i
+               existente = 1
+
+        if existente == 1 :
+           return [True , conta]       
+        else:
+          return [False, conta]
+     
     def consultaSaldo(self, numConta):
-        for conta in self.contas:
-            if conta.numero == numConta:
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca[0]  
+        if verifica == True: #caso a conta exista, consulta o saldo
                 return conta.saldo
-        return -1
+        else:
+            return -1 #caso a conta não exista
 
     def depositar(self, numConta, valor):
-        for conta in self.contas:
-            if conta.numero == numConta:
-                conta.deposite(valor)
-
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca [0]  
+        if verifica == True and (not isinstance(conta,contaBonificada)) and valor > 0: # caso a conta exista e nao seja bonificada
+           conta.deposite(valor)
+           return 2  
+        elif verifica == True and isinstance(conta,contaBonificada) and valor > 0: # caso a conta seja bonificada
+            conta.deposite(valor)
+            conta.bonus = valor * 0.0001 
+            return 1 
+        elif valor <= 0: #caso insira 0 ou algo negativo no valor do deposito
+            return -1       
+        elif verifica == False:
+         return 0        #caso a conta nao exista
+        
     def sacar(self, numConta, valor):
-        for conta in self.contas:
-            if conta.numero == numConta:
-                return conta.sacar(valor)
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca [0]  
+
+        if verifica == True:
+            return conta.sacar(valor)
+        else:
+            return -1     #conta nao existe
 
     def renderPoupanca(self, numConta):
-        for i in self.contas:
-            if i.numero == numConta and isinstance(i, Poupanca):
-                i.render()
-                return True
-        return False
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca [0] 
+
+        if verifica == False and conta == 0:#caso está conta não exista 
+                return False    
+        elif verifica == True and isinstance(conta, Poupanca) and conta.saldo > 0:#Caso consigo render a poupanca
+                conta.render()
+                return True 
+        elif  verifica == True and isinstance(conta,Poupanca) and conta.saldo == 0:   #caso a conta exista mas o saldo seja insuficiente  
+                return -1      
+        else:#caso a conta nao seja poupança
+                return -2 
+
+    def renderBonus(self, numConta):
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca [0] 
+
+        if verifica == False and conta == 0: #caso está conta não exista 
+                return False 
+        elif  verifica == True and isinstance(conta,contaBonificada) and conta.bonus == 0 :#caso a conta exista mas o bonus seja insuficiente  
+                print(conta.bonus) 
+                return -1
+        elif verifica == True and isinstance(conta, contaBonificada) and conta.bonus > 0: #caso consiga render o bonus
+                conta.render()
+                return True    
+        else: #caso a conta nao seja Bonificada
+                return -2 
+
+    def consultaBonus(self, numConta):
+        resultadoBusca = self.buscaConta(numConta)
+        conta = resultadoBusca[1]
+        verifica = resultadoBusca [0]  
+        if verifica == True and isinstance(conta, contaBonificada): #caso consiga consultar o bonus
+                return conta.bonus 
+        if verifica == False: #caso a conta nao exista
+                return -1       
+        return -2     #caso a conta exista mas nao seja do tipo bonificada           
+
+
+
+
--- main.py
+++ main.py
@@ -9,45 +9,97 @@ print("2 - Consultar Saldo Conta")
 print("3 - Depositar na Conta")
 print("4 - Sacar na Conta")
 print("5 - Render Poupanca")
-escolha = int(input("digite a opção desejada:"))
+print("6 - Render Bônus")
+print ("7 - Consultar Bônus")
+escolha = int(input("Digite a opção desejada:"))
 while escolha > 0:
     if escolha == 1:
         # criar uma conta
         print("Criando Conta...")
         print("1 - Conta Corrente")
         print("2 - Conta Poupanca")
+        print("3 - Conta Bonificada")
         opcao = int(input("digite o tipo da conta:"))
         if opcao == 1:
             numConta = bancoUfrpe.criarConta()
-        else:
+        if opcao == 2:
             numConta = bancoUfrpe.criarPoupanca()
+        if opcao == 3:
+            numConta = bancoUfrpe.criarContaBonificada()
         print("Conta criada:", numConta)
+        
     elif escolha == 2:
         print("Consultando Saldo...")
         numConta = int(input("digite o numero da conta:"))
-        saldo = bancoUfrpe.consultaSaldo(numConta)
-        print("o saldo da conta", numConta, "é", saldo, "R$")
+        resp = bancoUfrpe.consultaSaldo(numConta,valor)
+        if resp >= 0:
+         print("o saldo da conta", numConta, "é", resp, "R$")
+        else :
+          print("Esta conta não existe")  
+           
     elif escolha == 3:
         print("Depositando Conta...")
         numConta = int(input("digite o numero da conta:"))
         valor = int(input("digite o valor que deseja depositar:"))
-        saldo = bancoUfrpe.depositar(numConta, valor)
-        print("Valor Depositado")
+        resp = bancoUfrpe.depositar(numConta, valor)
+
+        if resp == 2 or resp == 1 : 
+             print("Valor Depositado")  
+        elif resp == -1:
+             print("Deposite acima de 0 R$")      
+        elif resp == 0 :
+             print("Esta conta não existe")
+
+
     elif escolha == 4:
         print("Sacando da Conta...")
         numConta = int(input("digite o numero da conta:"))
         valor = int(input("digite o valor que deseja sacar:"))
         resp = bancoUfrpe.sacar(numConta, valor)
-        if resp:  # significa resp == True
+        if resp == True:  # significa resp == True
             print("Valor Sacado")
+        if resp == -1:
+            print("Esta conta não existe")    
         else:
             print("Saldo Insuficiente")
+
     elif escolha == 5:
         print("Rendendo Poupanca...")
-        numConta = int(input("digite o numero da conta poupanca:"))
+        numConta = int(input("digite o numero da Conta Poupança:"))
         resp = bancoUfrpe.renderPoupanca(numConta)
-        if resp:
+        if resp == True:
             print("Poupanca com novo saldo")
+        if resp == -1 :
+            print("Seu saldo é Insuficiente. Você precisa de saldo positivo para render, Deposite!!!")
+        if resp == -2 :  
+            print("Esta conta não é Poupança")      
         else:
-            print("A conta não é poupanca ou não existe")
-    escolha = int(input("digite a opção desejada:"))
+            print("A conta não existe")
+
+    if escolha == 6:
+        print("Rendendo Bônus...")
+        numConta = int(input("digite o numero da Conta Bonificada:"))
+        resp = bancoUfrpe.renderBonus(numConta)
+        if resp == True:
+            print("Conta Bonificada com novo saldo")
+        elif resp == -1:  
+            print("Seu bônus é Insuficiente. Para acumular Bônus, Deposite!!!")
+        elif resp == -2: 
+            print("Esta conta não é Bonificada") 
+        else:
+            print("A conta não existe")
+
+
+    if escolha == 7:
+        print("Consultando Bônus...")
+        numConta = int(input("Digite o numero da conta:"))
+        resp = bancoUfrpe.consultaBonus(numConta)
+        if resp == -2 :
+             print("Esta conta não é do tipo Bonificada!!")
+        if resp == -1:
+             print ("Esta conta não existe")
+        else :
+             print("O bônus da conta", numConta, "é", resp, "R$")  
+
+
+    escolha = int(input("Digite a opção desejada:"))                
